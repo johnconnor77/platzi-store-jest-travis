b07b844c9c630b831f6607769c9584d631f17a74
johnconnor77 <jfcalleh@unal.edu.co>
2020-09-26 18:59:20 -0500

    Upload de todos los archivos de proyecto para pruebas con travis

---
+++ src/__mocks__/ProviderMock.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createStore } from "redux";
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createBrowserHistory } from 'history';
+import initialState from "../initialState";
+import reducer from "../reducers";
+
+const store = createStore(reducer, initialState);
+const history = createBrowserHistory();
+
+// se pasan como hijos los componentes que se necesitan destructurar
+
+const ProviderMock = props => (
+  <Provider store={store}>
+    <Router history={history}>
+      {props.children}
+    </Router>
+  </Provider>
+);
+
+export default ProviderMock;
